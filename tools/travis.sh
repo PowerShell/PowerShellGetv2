b7ce5b247c6ae403fff13d8560067d783e3b13af
Manikyam Bavandla <manikyam.bavandla@microsoft.com>
2016-11-01 21:10:24 -0700

    Updating travish.sh

--- tools/travis.sh
+++ tools/travis.sh
@@ -1,5 +1,5 @@
 set -x
 ulimit -n 4096
 
-sudo powershell -c "Import-Module ./build.psm1; Install-Dependencies; Invoke-PowerShellGetTest;"
+sudo powershell -c "Import-Module ./tools/build.psm1; Install-Dependencies; Invoke-PowerShellGetTest;"
     

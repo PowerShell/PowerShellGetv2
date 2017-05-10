2cb2476550a315a90e8fb295332c94cb1cce3e39
Manikyam Bavandla <manikb@microsoft.com>
2017-05-10 14:20:58 -0700

    Made changes to broadcast the Environment variable changes, so that other processes pick changes to Environment variables without having to reboot or logoff/logon. Also updated the failing test cases.

--- PowerShellGet/PSModule.psm1
+++ PowerShellGet/PSModule.psm1
@@ -12199,6 +12199,53 @@ function Set-EnvironmentVariable
     {
         Microsoft.PowerShell.Management\Set-ItemProperty $Path -Name $Name -Value $Value
     }
+
+    # Broadcast the Environment variable changes, so that other processes pick changes to Environment variables without having to reboot or logoff/logon. 
+    Send-EnvironmentChangeMessage
+}
+
+# Broadcast the Environment variable changes, so that other processes pick changes to Environment variables without having to reboot or logoff/logon. 
+function Send-EnvironmentChangeMessage
+{
+    if($Script:IsWindows)
+    {
+        if (-not ('Microsoft.PowerShell.Commands.PowerShellGet.Win32.NativeMethods' -as [type]))
+        {
+            Add-Type -Namespace Microsoft.PowerShell.Commands.PowerShellGet.Win32 `
+                     -Name NativeMethods `
+                     -MemberDefinition @'
+                        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+                        public static extern IntPtr SendMessageTimeout(
+                            IntPtr hWnd,
+                            uint Msg,
+                            UIntPtr wParam,
+                            string lParam,
+                            uint fuFlags,
+                            uint uTimeout,
+                            out UIntPtr lpdwResult);
+'@
+        }
+
+        $HWND_BROADCAST = [System.IntPtr]0xffff
+        $WM_SETTINGCHANGE = 0x1a
+        $result = [System.UIntPtr]::zero
+
+        $returnValue = [Microsoft.PowerShell.Commands.PowerShellGet.Win32.NativeMethods]::SendMessageTimeout($HWND_BROADCAST, 
+                                                                                                            $WM_SETTINGCHANGE,
+                                                                                                            [System.UIntPtr]::Zero, 
+                                                                                                            'Environment',
+                                                                                                            2, 
+                                                                                                            5000,
+                                                                                                            [ref]$result)
+        if($returnValue)
+        {
+            Write-Verbose -Message $LocalizedData.SentEnvironmentVariableChangeMessage
+        }
+        else
+        {
+            Write-Warning -Message $LocalizedData.UnableToSendEnvironmentVariableChangeMessage
+        }
+    }
 }
 
 function DeSerialize-PSObject

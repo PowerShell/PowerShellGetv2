66ddc6489941a63700b768208c02e862ab85cfb3
benny1007 <ben@mailinator.com>
2018-03-27 20:22:58 +0800

    updated build scripts appveyor and travis

--- tools/travis.sh
+++ tools/travis.sh
@@ -7,12 +7,14 @@ if [ $TRAVIS_EVENT_TYPE = cron ] || [ $TRAVIS_EVENT_TYPE = api ]; then
     sudo pwsh -c "Import-Module ./tools/build.psm1;
                   Install-Dependencies;
                   Update-ModuleManifestFunctions;
-                  Invoke-PowerShellGetTest;
-                  Publish-ModuleArtifacts -IsFullTestPass;"
+                  Publish-ModuleArtifacts;
+                  Install-PublishedModule;
+                  Invoke-PowerShellGetTest;"
 else
     sudo pwsh -c "Import-Module ./tools/build.psm1;
                   Install-Dependencies;
                   Update-ModuleManifestFunctions;
-                  Invoke-PowerShellGetTest;
-                  Publish-ModuleArtifacts"
+                  Publish-ModuleArtifacts;
+                  Install-PublishedModule;
+                  Invoke-PowerShellGetTest;"
 fi

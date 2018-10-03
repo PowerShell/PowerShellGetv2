ca739b74d7cbbd13299cd21a7611e4da272c9c13
edwin young <edwin@bathysphere.org>
2018-10-02 23:32:03 -0700

    remove func moved to separate file

--- PowerShellGet/private/modulefile/PartTwo.ps1
+++ PowerShellGet/private/modulefile/PartTwo.ps1
@@ -26,37 +26,6 @@ if(-not (Microsoft.PowerShell.Management\Test-Path -Path $script:MyDocumentsInst
                                                      -WhatIf:$false
 }
 
-function Add-ArgumentCompleter()
-{
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory=$true)]
-        [string[]]$cmdlets,
-        
-        [Parameter(Mandatory=$true)]
-        [string]$parameterName
-    )
-
-    try
-    {
-        if(Get-Command -Name Register-ArgumentCompleter -ErrorAction SilentlyContinue)
-        {
-            Register-ArgumentCompleter -CommandName $cmdlets -ParameterName $parameterName -ScriptBlock {
-                param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter) 
-                
-                Get-PSRepository -Name "$wordTocomplete*"-ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Foreach-Object { 
-                    [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name) 
-                } 
-           }
-        }
-    }
-    catch 
-    {
-        # All this functionality is optional, so suppress errors 
-        Write-Debug -Message "Error registering argument completer: $_"      
-    }
-}
-
 # allow -repository params to be tab-completed
 $commandsWithRepositoryParameter = @(
     "Find-Command"

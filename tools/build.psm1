bc3363e2b054063a4bbda406cf2d91a8bee554a2
edwin young <edwin@bathysphere.org>
2018-10-07 13:38:35 -0700

    tweaks

--- tools/build.psm1
+++ tools/build.psm1
@@ -305,13 +305,13 @@ function Test-DailyBuild {
 }
 function New-ModulePSMFile {
     $moduleFile = New-Item -Path $ArtifactRoot\PowerShellGet\PSModule.psm1 -ItemType File -Force
-    
-   # Add the localized data
+
+    # Add the localized data
     'Import-LocalizedData LocalizedData -filename PSGet.Resource.psd1' | Out-File -FilePath $moduleFile
     # Add the first part of the distributed .psm1 file from template.
     Get-Content -Path "$ModuleRoot\private\modulefile\PartOne.ps1" | Out-File -FilePath $moduleFile -Append
 
-    # Add a region and write out the private functi
+    # Add a region and write out the private functions.
     "`n#region Private Functions" | Out-File -FilePath $moduleFile -Append
     Get-Content $PrivateFunctions | Out-String | Out-File -FilePath $moduleFile -Append
     "#endregion`n" | Out-File -FilePath $moduleFile -Append

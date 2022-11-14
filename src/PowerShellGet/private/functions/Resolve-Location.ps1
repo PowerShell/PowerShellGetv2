6abef2760dcd5ad0fd8628dba65c982f1013d190
Amber Erickson <americks@microsoft.com>
2022-11-14 11:37:34 -0800

    Handle 403 and 405 status code in Resolve-Location

--- src/PowerShellGet/private/functions/Resolve-Location.ps1
+++ src/PowerShellGet/private/functions/Resolve-Location.ps1
@@ -1,14 +1,13 @@
-function Resolve-Location
-{
+function Resolve-Location {
     [CmdletBinding()]
     [OutputType([string])]
     Param
     (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [string]
         $Location,
 
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [string]
         $LocationParameterName,
 
@@ -27,54 +26,47 @@ function Resolve-Location
     )
 
     # Ping and resolve the specified location
-    if(-not (Test-WebUri -uri $Location))
-    {
-        if(Microsoft.PowerShell.Management\Test-Path -LiteralPath $Location)
-        {
+    if (-not (Test-WebUri -uri $Location)) {
+        if (Microsoft.PowerShell.Management\Test-Path -LiteralPath $Location) {
             return $Location
         }
-        elseif($CallerPSCmdlet)
-        {
+        elseif ($CallerPSCmdlet) {
             $message = $LocalizedData.PathNotFound -f ($Location)
             ThrowError -ExceptionName "System.ArgumentException" `
-                       -ExceptionMessage $message `
-                       -ErrorId "PathNotFound" `
-                       -CallerPSCmdlet $CallerPSCmdlet `
-                       -ErrorCategory InvalidArgument `
-                       -ExceptionObject $Location
+                -ExceptionMessage $message `
+                -ErrorId "PathNotFound" `
+                -CallerPSCmdlet $CallerPSCmdlet `
+                -ErrorCategory InvalidArgument `
+                -ExceptionObject $Location
         }
     }
-    else
-    {
+    else {
         $pingResult = Ping-Endpoint -Endpoint $Location -Credential $Credential -Proxy $Proxy -ProxyCredential $ProxyCredential
         $statusCode = $null
         $exception = $null
         $resolvedLocation = $null
-        if($pingResult -and $pingResult.ContainsKey($Script:ResponseUri))
-        {
+        if ($pingResult -and $pingResult.ContainsKey($Script:ResponseUri)) {
             $resolvedLocation = $pingResult[$Script:ResponseUri]
         }
 
-        if($pingResult -and $pingResult.ContainsKey($Script:StatusCode))
-        {
+        if ($pingResult -and $pingResult.ContainsKey($Script:StatusCode)) {
             $statusCode = $pingResult[$Script:StatusCode]
         }
 
         Write-Debug -Message "Ping-Endpoint: location=$Location, statuscode=$statusCode, resolvedLocation=$resolvedLocation"
 
-        if((($statusCode -eq 200) -or ($statusCode -eq 401) -or ($statusCode -eq 407)) -and $resolvedLocation)
-        {
+        if ((($statusCode -eq 200) -or ($statusCode -eq 401) -or ($statusCode -eq 403) -or ($statusCode -eq 405) -or ($statusCode -eq 407)) `
+                -and $resolvedLocation) {
             return $resolvedLocation
         }
-        elseif($CallerPSCmdlet)
-        {
+        elseif ($CallerPSCmdlet) {
             $message = $LocalizedData.InvalidWebUri -f ($Location, $LocationParameterName)
             ThrowError -ExceptionName "System.ArgumentException" `
-                       -ExceptionMessage $message `
-                       -ErrorId "InvalidWebUri" `
-                       -CallerPSCmdlet $CallerPSCmdlet `
-                       -ErrorCategory InvalidArgument `
-                       -ExceptionObject $Location
+                -ExceptionMessage $message `
+                -ErrorId "InvalidWebUri" `
+                -CallerPSCmdlet $CallerPSCmdlet `
+                -ErrorCategory InvalidArgument `
+                -ExceptionObject $Location
         }
     }
 }
